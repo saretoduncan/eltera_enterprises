f29eccc8b82de05b76935284234d2e0ef6faff72
Duncan Moiyo <duncan.moiyo@gmail.com>
2024-08-26 08:38:08 +0300

    add animation to hero section

--- src/pages/homePage/heroSection.tsx
+++ src/pages/homePage/heroSection.tsx
@@ -1,16 +1,27 @@
-import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
 import roadLorries from "../../assets/road_lorries.webp";
 import { NavLinks } from "../../types/navigation_types";
 import SecondaryLinkButton from "../../components/Buttons/SecondaryLinkButton";
 import TertiaryLinkButton from "../../components/Buttons/TertiaryLinkButton";
 const HeroSection = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
   return (
     <div
       className={`w-full h-[32rem] md:h-[34rem] lg:h-[30rem] xl:h-[34rem] relative bg-cover bg-no-repeat bg-center grid`}
       style={{ backgroundImage: "url(" + "'" + roadLorries + "'" + ")" }}
     >
       <div className="absolute top-0 bottom-0 w-full bg-gradient-to-r from-primaryBlue to-[#11111170] grid px-4 lg:px-8 ">
-        <div className="self-end   text-white space-y-6 mb-12 lg:w-[1280px] xl:mx-auto">
+        <div
+          ref={ref}
+          className={`self-end   text-white space-y-6 mb-12 lg:w-[1280px] xl:mx-auto
+        translate-all  duration-1000 ease-in-out ${
+          inView ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
+        }
+        `}
+        >
           <section className="md:w-[400px] lg:w-[700px] space-y-6">
             <h1 className="text-white font-bold text-xl text-shadow shadow-black md:text-2xl lg:text-4xl">
               Efficient Goods Transportation Across Kenya with Reliable
