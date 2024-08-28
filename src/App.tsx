d40c7645ad0a7719eac111a0b575817b56fe1951
Duncan Moiyo <duncan.moiyo@gmail.com>
2024-08-28 06:25:30 +0300

    add nav bar opacity change custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import MainRouters from "./MainRoutes";
 import DropNavBar from "./components/navbar/DropNavBar";
 import Navbar from "./components/navbar/Navbar";
 import NavBarContextWrapper from "./contexts/NavBarContextWrapper";
-
+import useNavChangeHook from "./components/customHooks/useNavChangeHook";
 function App() {
+  const { isNavFull } = useNavChangeHook();
   return (
     <>
       {" "}
       <header className="relative">
         <NavBarContextWrapper>
-          <section className="bg-primaryBlue bg-opacity-80 w-full fixed z-20 h-[77px] lg:h-[100px] ">
+          <section
+            className={`bg-primaryBlue transition-all duration-500 ease-in-out ${
+              isNavFull ? "bg-opacity-100" : "bg-opacity-80"
+            }  w-full fixed z-20 h-[77px] lg:h-[100px] `}
+          >
             <Navbar />
           </section>
           <DropNavBar />
