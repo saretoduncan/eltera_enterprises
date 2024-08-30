bad7a007e60a83a0b26742953887755ca2dbe4a1
Duncan Moiyo <duncan.moiyo@gmail.com>
2024-08-30 22:15:35 +0300

    complete testimonial

---
+++ src/components/TestimonialCard.tsx
@@ -0,0 +1,40 @@
+import { FaQuoteLeft } from "react-icons/fa";
+import { TTestimonialType } from "../types/testimonial_types";
+const TestimonialCard: React.FC<TTestimonialType> = ({
+  companyTitle,
+  content,
+  imgUrl,
+  repName,
+}) => {
+  return (
+    <>
+      <div className="bg-white px-4 py-4 grid m-4 text-center  min-h-[550px] rounded text-gray-800 space-y-4 shadow shadow-secondary-color">
+        <div className=" flex justify-center">
+          <FaQuoteLeft className="text-secondary-color text-4xl" />
+        </div>
+        <div>
+          <div className=" space-y-4">
+            <p className=" text-sm">{content}</p>
+            <div>
+              <figure className="grid space-y-2">
+                <img
+                  src={imgUrl}
+                  alt="maisha magic ltd logo"
+                  className="w-[80px] justify-self-center"
+                />
+                <figcaption className="w-[200px] mx-auto">
+                  <p className="text-center text-sm">
+                    {repName} -{" "}
+                    <span className="text-secondary-color">{companyTitle}</span>
+                  </p>
+                </figcaption>
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TestimonialCard;
--- src/pages/homePage/testimonialSection.tsx
+++ src/pages/homePage/testimonialSection.tsx
@@ -1,6 +1,72 @@
+import TestimonialCard from "../../components/TestimonialCard";
+import Slider, { Settings } from "react-slick";
+import { TestimonialData } from "../../types/testimonial_types";
+
 
 const TestimonialSection = () => {
-  return <div>Testimonial Section</div>;
+  const sliderConfig: Settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    initialSlide: 0,
+    autoplay:true,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+    ],
+  };
+
+  return (
+    <>
+      <div className=" bg-gray-100 w-full py-8 overflow-hidden">
+        <h5 className="text-center text-primaryBlue text-lg font-bold mb-2">
+          <span className="text-sm text-secondary-color">WHAT THEY SAY</span>
+          <br /> Client's Testimonial
+        </h5>
+        <div className=" px-8 h-full xl:w-[1280px]  xl:mx-auto ">
+          <Slider {...sliderConfig}>
+            {TestimonialData.map((item, index) => (
+              <TestimonialCard
+                companyTitle={item.companyTitle}
+                content={item.content}
+                imgUrl={item.imgUrl}
+                repName={item.repName}
+                key={index}
+              />
+            ))}
+          </Slider>
+        </div>
+
+        {/*  */}
+      </div>
+    </>
+  );
 };
 
 export default TestimonialSection;
