9ce7719e7511f5dcc17e7e7f9809931faf9d006a
Duncan Moiyo <duncan.moiyo@gmail.com>
2024-09-01 21:53:28 +0300

    remove unnecesary imports

--- src/components/customHooks/useNavChangeHook.tsx
+++ src/components/customHooks/useNavChangeHook.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const useNavChangeHook = () => {
   const [isNavFull, SetNavFull] = useState(false);
